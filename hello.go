a58d2eb3383ca12ecb1313f3a2f5e89005557d15
chris-carpenter <cdcarpenter@gmail.com>
2020-02-17 11:20:40 -0600

    Add diff on hello world

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/chris-carpenter/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-	fmt.Println(morestrings.ReverseRunes("Hello, Go!"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))	
 }
